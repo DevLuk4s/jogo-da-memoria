a375523f6405ccd3e732fb56d7caa6feafcc3f38
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-08-24 20:23:09 -0300

    jogo-da-memoria

---
+++ js/login.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.login_input')
+const button = document.querySelector('.login_button')
+const form = document.querySelector('.login-form')
+
+const validadoInput = ({ target} ) => {
+    if(target.value.length > 2) {
+        button.removeAttribute('disabled')
+    } else {
+        button.setAttribute('disabled', '')
+    }
+}
+
+const enviarNome = (event) => {
+    event.preventDefault()
+    
+    localStorage.setItem('player', input.value)
+    window.location = 'pages/games.html'
+}
+
+
+input.addEventListener('input', validadoInput)
+form.addEventListener('submit', enviarNome)
